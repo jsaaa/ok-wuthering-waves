84855ff678d35f5260ff0ce8639162f96be234dc
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-05-04 23:08:00 +0800

    修复可能无法重击

--- tests/TestForte.py
+++ tests/TestForte.py
@@ -26,6 +26,16 @@ class TestForte(TaskTestCase):
         # self.task.sleep(1)
         self.assertIsNotNone(forte)
 
+    def test_forte4(self):
+        self.task.do_reset_to_false()
+        self.set_image('tests/images/33_forte_2.png')
+        self.task.load_chars()
+        forte = self.task.find_mouse_forte()
+        self.task.sleep(1)
+        self.task.screenshot('forte', show_box=True)
+        self.task.sleep(1)
+        self.assertIsNotNone(forte)
+
     def test_forte3(self):
         self.task.do_reset_to_false()
         self.set_image('ok_templates/3.png')
